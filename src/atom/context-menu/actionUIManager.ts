d9da6a99ba7f1bba1d0625b832299f4195c31b9e
Denis Denisenko <denis@onpositive.com>
2017-11-14 19:54:57 +0700

    More actions.

--- src/atom/context-menu/actionUIManager.ts
+++ src/atom/context-menu/actionUIManager.ts
@@ -1,6 +1,7 @@
 import completeBodyUI = require("./actions/completeBody/ui")
 import newMethodUI = require("./actions/newMethod/ui")
-import newResponseUI = require("./actions/newResponse/ui")
+
+import simpleActionUi = require("./actions/simpleAction/ui")
 
 interface ActionUI {
     run(initialState?: any): Promise<any>;
@@ -11,13 +12,41 @@ export function getUICode(actionID: string): ActionUI {
         return completeBodyUI;
     }
 
-    if(actionID == "Create new Response") {
-        return newResponseUI;
-    }
-
     if(actionID == "newMethod") {
         return newMethodUI;
     }
-    
+
+    if(actionID == "Create new Response") {
+        return simpleActionUi;
+    }
+
+    if(actionID == "Create new URI Parameter") {
+        return simpleActionUi;
+    }
+
+    if(actionID == "Create new Query Parameter") {
+        return simpleActionUi;
+    }
+
+    if(actionID == "Create new Header") {
+        return simpleActionUi;
+    }
+
+    if(actionID == "Create new Response Header") {
+        return simpleActionUi;
+    }
+
+    if(actionID == "Create new Response Body") {
+        return simpleActionUi;
+    }
+
+    if(actionID == "Create new Property") {
+        return simpleActionUi;
+    }
+
+    if(actionID == "Create new Body") {
+        return simpleActionUi;
+    }
+
     return null;
 }
